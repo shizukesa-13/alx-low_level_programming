daec9446cfd5250bbc143635bbc7deabdcada5e8
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-15 17:55:39 -0700

    HW04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A C program that prints a line using the function puts
- * Return: 0 (success) 
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
