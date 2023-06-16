ac72542db591f8a9675d0f944881f54bd6a536b8
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-15 19:09:31 -0700

    HW101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line using the function char 
+ * Retur: 1 (Error)
+ */
+int main(void)
+{
+char printing[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+return (1);
+}
