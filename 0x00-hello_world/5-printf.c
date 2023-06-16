a6cfc62a09d68d6a72227f8aad1f8072eea5eb5a
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-15 18:03:29 -0700

    HW05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A c program that prints a line using the funtion printf
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
