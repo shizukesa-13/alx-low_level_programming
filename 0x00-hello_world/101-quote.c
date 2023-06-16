71c6b4f67d493a390cb2e775390ec8e05f829c4c
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-15 19:25:20 -0700

    HW101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - A C program that prints a line using the function char 
  * Retur: 1 (Error)
  */
 int main(void)
 {
-char printing[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+char printing[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+write(1, printing, sizeof(printing) - 1);
 return (1);
 }
