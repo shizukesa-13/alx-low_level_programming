8191b3ea1b8ddd7d7813713eedf84ab4b3f140e1
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-15 17:36:04 -0700

    HW04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+ * writing a C program that prints exactly "Programming is like building a multilingual puzzle with puts 
+ * /
 int main(void)
 {
-       	puts("Programming is like building a multilingual puzzle"); 
+       	puts(""Programming is like building a multilingual puzzle"); 
 	return 0;
 }
