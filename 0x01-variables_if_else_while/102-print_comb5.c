1c07b172a22e0f4130b84d5eb95d929a08fedb26
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 19:49:15 -0700

    C-View101

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - The 10 digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+for (x = 0; x <= 99 ; x++)
+{
+for (y = x + 1; y <= 99; y++)
+{
+putchar((x / 10) + '0');
+putchar((x % 10) + '0');
+putchar(' ');
+putchar((y / 10) + '0');
+putchar((y % 10) + '0');
+if (x != 99 || y != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
