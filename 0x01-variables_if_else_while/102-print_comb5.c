40ba1ad16c73a2c4a6d50507e135efcf149cd7b7
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 20:00:47 -0700

    cv

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,19 +6,22 @@
 int main(void)
 {
 int x, y;
-for (x = 0; x <= 99 ; x++)
+for (x = 0; x < 100 ; x++)
 {
-for (y = x + 1; y <= 98; y++)
+for (y = x + 1; y < 100; y++)
+{
+if (x < y)
 {
 putchar((x / 10) + '0');
 putchar((x % 10) + '0');
 putchar(' ');
 putchar((y / 10) + '0');
 putchar((y % 10) + '0');
-if (x != 99 || y != 99)
+if (x != 98 || y != 99)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
