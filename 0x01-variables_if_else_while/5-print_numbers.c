2e0ec4158c8d6c8e2d3ffd31f2b18f8c7416a599
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 18:27:41 -0700

    C-View5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char dig[10] = "0123456789";
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(alph[x]);
+}
+putchar('\n');
+return (0);
+}
