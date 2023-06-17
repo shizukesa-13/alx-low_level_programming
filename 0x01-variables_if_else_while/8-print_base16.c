1f32f019d93557aaade16f778c67c17beda75721
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 18:50:10 -0700

    C-View8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char num[16] = "0123456789ABCDEF";
+int x;
+for (x = 0; x < 16; x++)
+{
+putchar(num[x]);
+}
+putchar('\n');
+return (0);
+}
