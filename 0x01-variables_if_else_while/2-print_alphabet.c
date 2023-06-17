2e5f142b0a64b5167c9446a47b6efcd878a09cd6
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 18:05:22 -0700

    C-View2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabets
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int x;
+for (i = 0; x < 26; x++);
+{
+putchar(alph[x]);
+}
+putchar("\n");
+return (0);
+}
