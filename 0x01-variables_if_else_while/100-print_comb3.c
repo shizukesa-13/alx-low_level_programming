2656a8f22d845d528c317c1796ba74a81ee548b6
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 19:28:19 -0700

    C-View100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - The 10 digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+for (x = 0; x < 9; x++)
+{
+for (y = x +1; x <= 9; y+++)
+{
+putchar(x + '0')
+putchar(y +'0')
+if (x != 8 || y != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
