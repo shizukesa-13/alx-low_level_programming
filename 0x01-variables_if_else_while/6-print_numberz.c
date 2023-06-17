e7304cffff181f2882e4674ff7e2d4a7d7c57a5d
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-16 18:36:55 -0700

    C-View6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - lowercase alphabets
+ * main - The 10 digits
  * Return: 0 (Success)
  */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 int x;
 for (x = 0; x < 10; x++)
 {
-putchar(x);
+putchar(x + '0');
 }
 putchar('\n');
 return (0);
