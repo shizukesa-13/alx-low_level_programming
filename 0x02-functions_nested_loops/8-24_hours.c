76a234f2eb0ee28f665f740a5a0c50b155671360
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 03:06:47 +0100

    C-Fnl7

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: y value
+ */
+void jack_bauer(void)
+{
+int x,y;
+for (x = 0; x < 24; x++)
+{
+for (y = 0; y < 60; y++)
+{
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0');
+_putchar(':');
+_putchar(y / 10 + '0');
+_putchar(y % 10 + '0');
+_putchar('\n');
+}
+}
+}
