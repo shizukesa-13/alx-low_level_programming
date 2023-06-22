ae10ca52b94b608c44e824346d82efa2f985f445
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 03:22:17 +0100

    C-Fnl9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - prints every minute of the day of Jack Bauer
+ * Return: y value
+ */
+void times_table(void)
+{
+int x, y, z;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
+if (y == 0)
+{
+_putchar('0');
+}
+else if (z < 10)
+{
+_putchar(' ');
+_putchar('0' + z);
+}
+else
+{
+_putchar('0' + z / 10);
+_putchar('0' + z % 10);
+}
+if (y != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n')
+}
+}
+}
+}
