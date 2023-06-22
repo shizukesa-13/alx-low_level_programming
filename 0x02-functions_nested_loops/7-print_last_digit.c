cc33dfbfce1a520364c2a3b06a4dbb1503eb9a2a
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 02:47:47 +0100

    C-Fnl7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: the character to be checked
+ * Return: y value
+ */
+int print_last_digit(int)
+{
+int y;
+y = x % 10;
+if (y < 0)
+y = -y;
+_putchar(y + '0');
+return (y);
+}
