dc0d702f6757a6963fa22855c46e4b03fac1f09f
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 03:31:21 +0100

    C-Fnl10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
+ * @a : var 1
+ * @b : var 2
  * Return: 0 (Success)
  */
 int add(int a, int b)
 {
 int adds;
-adds = a +b;
-return adds;
+adds = a + b;
+return (adds);
 }
