32ae7bf4f3d8072f0695391fe46d5a55376ce2ce
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 02:10:08 +0100

    C-Fnl3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - lowercase alphabets with _putchar
+ * Return: 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
