b1cc7fb8797bf7ffcd6c61d5778b47a49a577a73
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-22 02:37:32 +0100

    C-Fnl6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @x: the character to be checked
+ * Return: Absolute value of x
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+return -x;
+}
+else
+{
+return x;
+}
+}
