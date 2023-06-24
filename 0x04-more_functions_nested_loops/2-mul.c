fdf6955a31bb57304af7830687ba60f87e21c0e3
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-24 02:28:04 +0100

    C-MoreFnl2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: character 1
+ * @b: character 2
+ * Return: result of *
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return result;
+}
