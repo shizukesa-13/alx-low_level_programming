f45f469d9d424441abe8fae86b8811ec6d93f1fc
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-24 02:13:23 +0100

    C-MoreFnl0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _isupper - function that checks for uppercase character
+ * _isupper - function that checks for uppercase character specificly the letter c
  * @c: character
  * Return: 1 id c is uppercase else 0
  */
