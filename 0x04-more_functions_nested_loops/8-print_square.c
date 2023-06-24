ca12ff84d12278f6fdef671939211e7f45f3090c
Safaa ABDELHAKIM <itssafaaabdelhakim@gmail.com>
2023-06-24 03:40:31 +0100

    C-MoreFnl8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: square size
+ */ 
+void print_square(int size)
+{
+int x, y;
+y = 0;
+if (size < 1)
+_putchar('\n');
+while (y < size)
+{
+x = 0;
+while (x < size)
+{
+_putchar('#');
+x++;
+}
+_putchar('\n');
+y++;
+}
+}
